1d737c8e28f6e0205548492b7e52fbce0f558c1c
MMarcin89 <mmarcin89@o2.pl>
2017-08-30 21:33:22 +0200

    using GetTimeSeconds

--- RoomEscape/Source/RoomEscape/OpenDoor.cpp
+++ RoomEscape/Source/RoomEscape/OpenDoor.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "OpenDoor.h"
-#include "Gameframework/Actor.h"
+
+#include "GameFramework/Actor.h"
 
 
 
@@ -20,18 +21,25 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
+	//Find owning actor
+	Owner = GetOwner();
 	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
 void UOpenDoor::OpenDoor()
 {
-	//Find owning actor
-	AActor *Owner = GetOwner();
+	
 	//Creat rotator
-	FRotator NewRotation = FRotator(0, -90, 0);
+	FRotator NewRotation = FRotator(0, OpenAngle, 0);
 	//Set door rotation
 	Owner->SetActorRotation(NewRotation);
 
+}
+
+void UOpenDoor::CloseDoor()
+{
+	//Set door rotation
+	Owner->SetActorRotation(FRotator(0, 0, 0)); //or Creat rotator (look OpenDoor)							
 }
 
 
@@ -44,10 +52,15 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	// if the actorThahOpens is in the volume than we open the door
 	if (PressurePoint->IsOverlappingActor(ActorThatOpens))
 	{
-		
-		OpenDoor();
-	}
 
+		OpenDoor();
+		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+	}
+	//else if (!PressurePoint->IsOverlappingActor(ActorThatOpens) && (LastDoorOpenTime + DoorCloseDelay <= GetWorld()->GetTimeSeconds()))
+	if(GetWorld()->GetTimeSeconds()-LastDoorOpenTime>DoorCloseDelay)
+	{
+		CloseDoor();
+	}
 }
 
 
