0d85cdd5049766c540e58e16520e157737a22379
MMarcin89 <mmarcin89@o2.pl>
2017-08-31 15:41:00 +0200

    DrawDebug line in front of our playerviepoint

--- RoomEscape/Source/RoomEscape/Grabber.cpp
+++ RoomEscape/Source/RoomEscape/Grabber.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Grabber.h"
-
+#include "DrawDebugHelpers.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -32,12 +32,26 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	// get player view point this tick
 	FVector OwnerLocation;
 	FRotator OwnerRotator;
-	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OwnerLocation, OwnerRotator);
+	 GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OwnerLocation, OwnerRotator);
 	
 	//UE_LOG(LogTemp, Warning, TEXT("Pozycja %s , rotacja %s"), *OwnerLocation.ToString(),*OwnerRotator.ToString());
 
 	//draw a red trace in the world to visualize
 
+	 FVector TraceLineEnd = OwnerLocation + (OwnerRotator.Vector()*Reach);
+	
+	DrawDebugLine
+	(
+		GetWorld(),
+		OwnerLocation,
+		TraceLineEnd,
+		FColor(255, 0, 0),
+		false,
+		0,
+		0,
+		10
+
+		);
 	// ray-cast out to rich distance
 
 	//see what we hit
